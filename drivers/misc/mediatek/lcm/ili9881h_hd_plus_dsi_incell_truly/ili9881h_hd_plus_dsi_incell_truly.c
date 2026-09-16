13ee98deda4d87803aa96fe4ab6ecc250b922fe0
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2026-09-16 20:38:43 +0700

    Fix brightness not work

--- drivers/misc/mediatek/lcm/ili9881h_hd_plus_dsi_incell_truly/ili9881h_hd_plus_dsi_incell_truly.c
+++ drivers/misc/mediatek/lcm/ili9881h_hd_plus_dsi_incell_truly/ili9881h_hd_plus_dsi_incell_truly.c
@@ -368,7 +368,7 @@ static void push_table(void *cmdq, struct LCM_setting_table *table,
 
 		default:
 			if (lcm_util.dsi_set_cmdq_V22)
-				dsi_set_cmdq_V22(cmdq, cmd, table[i].count, table[i].para_list, force_update);
+				lcm_util.dsi_set_cmdq_V22(cmdq, cmd, table[i].count, table[i].para_list, force_update);
 		}
 	}
 }
